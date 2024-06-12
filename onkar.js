b59aaaf1d4af20333b1b26a6067bc42deb4e6720
onkar <onkarsawant111@gmail.com>
2024-06-12 11:27:45 +0530

    updated

--- onkar.js
+++ onkar.js
@@ -1,2 +1,116 @@
-let a = 5
-console.log(a);
+// a = {name:"onkar",
+//     age:[24,26,15,2,9,1,3,6,5]
+// }
+// console.log(a.age[1]);
+
+// a = {name:{fullname:{firstname:"onkar", lastname:"sawant"},age:[122,566,2,26,2,1,511,1,15]}}
+// console.log(a.name.fullname.firstname);
+// console.log(a.name.age[6]);
+// console.log(a["name"]["age"]);
+
+// function sum(x,y){
+//     return x + y
+// }
+// console.log(sum(2,22));
+
+// let a = (x,y)=>{
+//     return x + y
+// }
+// console.log(a(2,5));
+
+// a = function(x,y){
+//     return x + y
+// }
+// console.log(a(2,4));
+
+// let a = 1
+// console.log(a++);
+// console.log(a);
+
+// let a = 1
+// console.log(++a);
+// console.log(a);
+
+// let a = "5"
+// b = parseFloat(a)
+// console.log(b);
+
+// a = 5
+// console.log(a);
+
+// a = null
+// console.log(isNaN(a));
+
+// a = "onkar"
+// console.log(a.length);
+
+// a = [15,1,22,5,66,778]
+// a.pop()
+// a.push(25)
+// console.log(a);
+// b = ["onkar","taj"]
+// console.log(a.concat(b));
+
+// a = 2.1
+// console.log(Math.ceil(a));  
+
+// b = "onkar"
+// c = 24
+// a = `my name is ${b} and my age is ${c}`
+// console.log(a);
+
+// a = [15,1,22,5,66,778,"onkar","raj"]
+// for (i of a){
+//     console.log(i);
+// }
+// for (i in a){
+//     console.log(a[i]);
+// }
+
+// a = [15,1,22,5,66,778,"onkar","raj"]
+// i = 0
+// while(i < a.length){
+//     console.log(a[i]);
+//     i = i + 1
+// }
+// for( let i = 0; i < a.length; i++){
+//     console.log(a[i]);
+// }
+// let i = 0
+// do{
+//     console.log("hi");
+//     i = i + 1
+// }while(i > a.length)
+
+// a = [1,2,3,4,5,6,88,8,9]
+
+// a.forEach(e => {
+//     console.log(e + 5);
+// });
+    
+// a.forEach(e=>{
+//     if(e % 2 == 0){
+//         console.log(e)
+//     }
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
